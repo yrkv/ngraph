95984720f67732ea8e93a28d174122e351416525
bunnalec <bunnalec@yahoo.com>
2023-09-03 12:24:34 -0700

    Another small bug fix

--- NeuralGraph.py
+++ NeuralGraph.py
@@ -602,7 +602,7 @@ class NeuralGraph(nn.Module):
         torch.save(self.state_dict(), path)
     
     def load(self, path):
-        self.load_state_dict(torch.load(path))
+        self.load_state_dict(torch.load(path, map_location=self.device))
 
     def plot(self):
         g = nx.DiGraph()
